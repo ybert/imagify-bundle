91017517f8ae740232a1ae980ec89e21961f7fc3
ybert <yohann.berthon@gmail.com>
2016-10-25 15:45:03 +0200

    AppKernel integration

---
+++ YbertImagifyBundle.php
@@ -0,0 +1,16 @@
+<?php
+namespace Ybert\ImagifyBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YbertImagifyBundle extends Bundle
+{
+    /**
+     * @see Symfony\Component\HttpKernel\Bundle.Bundle::build()
+     */
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
